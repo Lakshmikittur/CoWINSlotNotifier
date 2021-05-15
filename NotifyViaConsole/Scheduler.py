5f6b8b2e291b927b9abf11fb449af63dabb787e1
Lakshmi Kittur <kittur.lakshmi@gmail.com>
2021-05-15 12:56:55 +0530

    A helper script to run slot finder script every 10 seconds

---
+++ NotifyViaConsole/Scheduler.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import time
+import os
+import subprocess
+
+# Run slot finding script every 10 seconds
+while True:
+    cmd = ["python3", "FindSlots.py"]
+    if os.name == 'nt':
+        cmd[0] = "python"
+    result = subprocess.run(cmd)
+    time.sleep(10)
